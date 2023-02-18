26945a48bc16359eec0aee4367290839a0e5c0c6
danu <danujagreru@gmail.com>
2023-02-18 15:32:15 +0530

    demo exception

--- exception-04/src/Demo.java
+++ exception-04/src/Demo.java
@@ -5,6 +5,18 @@
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hello!");
+        System.out.println("start main");
+
+        int i = 10;
+        int j = 0;
+
+        try {
+            int k = i/j;
+            System.out.println(k);
+        }catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("end main");
     }
 }
